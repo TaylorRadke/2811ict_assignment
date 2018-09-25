b211472f4220935931f19ffe7231251d5bc7d7fc
Ghoustdownunder <taylorradke98@gmail.com>
2018-09-25 20:26:52 +1000

    Added mongo connection with server

--- server/app.js
+++ server/app.js
@@ -2,38 +2,30 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-const fs = require('fs');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const MongoDB = require("mongodb").MongoClient;
+const URL = "mongodb://localhost:27017/mydb";
 
 //Apply middleware to app
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname,'../app-chat/dist/app-chat/')));
+//app.use(express.static(path.join(__dirname,'../app-chat/dist/app-chat/')));
 
-//Create the file on server start which writes the initial super user with super user permissions
-//If the there is an error such as the file not existing
-fs.readFile('./resources/users.json','utf-8',function(err){
-    if (err){
-        fs.writeFile('./resources/users.json',JSON.stringify([{'username':'super','permissions':"super"}]));
-    }
+MongoDB.connect(URL,function(err,db){
+    if (err) throw err;
+    dbo = db.db('Chat_app_db');
+    console.log("Connected to db");
+    require('./routes/users')(app,dbo);
+    // require("./routes/groups")(app,dbo);
+    // require('./routes/channels.js')(app,dbo);
 });
 
-//Create the file on server start which writes initial empty array of groups if there is an error
-fs.readFile('./resources/groups.json','utf-8',function(err){
-    if (err){
-        fs.writeFile('./resources/groups.json',JSON.stringify([]),{encoding:'utf-8'});
-    }
-});
-
-require('./routes/users.js')(app,fs);
-require("./routes/groups.js")(app,fs);
-require('./routes/channels.js')(app,fs);
 
 //Start server on port 3000;
 http.listen(3000);
 
 //Wild card route for if the user enters an invalid url for server, serves frontend
-app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname,"../app-chat/dist/app-chat/index.html"));
-})
+// app.all('*',function(req,res){
+//     res.sendFile(path.join(__dirname,"../app-chat/dist/app-chat/index.html"));
+// })
